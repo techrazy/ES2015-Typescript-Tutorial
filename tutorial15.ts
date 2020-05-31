4baa269aad8b077e75d070dc49f618c5c3020fe7
Anu <sanurag22@gmail.com>
2020-05-31 18:01:42 +0530

    practice

--- tutorial15.ts
+++ tutorial15.ts
@@ -14,14 +14,17 @@ function createPerson(firstname, lastname, age){
         fullname,
         isSenior(){
             return age>60;
-        }}
+        },
+        isJunior: () => age<18
+    }
 }
 
-let p = createPerson("Ross","Geller", 62);
+let p = createPerson("Ross","Geller", 34);
 console.log(p.firstname);
 console.log(p.lastname);
 console.log(p.fullname);
 console.log(p.isSenior());
+console.log(p.isJunior());
 
 
 
